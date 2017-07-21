128eef88277cef1ee2b33f2c3a791253539345c5
Mardini <mardinid@amazon.com>
2017-07-21 16:53:31 -0700

    First commit.

---
+++ TravelPlanner/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TravelPlanner
+//
+//  Created by Mardini, Daniel on 7/18/17.
+//  Copyright © 2017 Mardini, Daniel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
